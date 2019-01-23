c71491bf3491563d37dccea86c7653c2fe728c03
Tyler Hogsett <tyhogset@microsoft.com>
2019-01-22 21:15:08 -0700

    Add project files.

---
+++ Stubify/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Stubify
+{
+    class Program
+    {
+        private static CrmServiceClient _client;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (_client = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMConnectionString"].ConnectionString))
+                {
+                    //Do stuff
+                }
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                string message = ex.Message;
+                throw;
+            }
+        }
+    }
+}
